a3cc2182f3595e855a4104196b643b61e17f052d
Yohannes18 <124924976+Yohannes18@users.noreply.github.com>
2023-12-02 07:49:04 -0600

    Add files via upload

---
+++ GDSC_study_session_g2/H.PY
@@ -0,0 +1,24 @@
+x = 12
+print(type(x))
+
+z = "ab"
+print(type(z))
+
+y = False
+print(type(y))
+ 
+m = None
+print(type(m))
+
+x += 1
+print(x)
+
+a = 2
+print(z * a)
+
+print(x ** a)
+
+t = 13
+print(x < t)
+
+
